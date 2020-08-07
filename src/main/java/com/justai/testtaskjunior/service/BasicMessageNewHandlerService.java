53faf8a561c69a2a180c1619109d507b7932986f
cptbronzebeard <omg4itep@gmail.com>
2020-08-07 06:21:54 +0300

    Removed duplicated error-checking logic from BasicMessageNewHandlerService

--- src/main/java/com/justai/testtaskjunior/service/BasicMessageNewHandlerService.java
+++ src/main/java/com/justai/testtaskjunior/service/BasicMessageNewHandlerService.java
@@ -1,11 +1,8 @@
 package com.justai.testtaskjunior.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.justai.testtaskjunior.model.messages.VkMessage;
 import com.justai.testtaskjunior.model.queries.MessageSendQuery;
-import com.justai.testtaskjunior.util.exceptions.ErroneousResponseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -22,22 +19,20 @@ public class BasicMessageNewHandlerService implements ObjectHandler<VkMessage> {
 
     @Autowired
     public BasicMessageNewHandlerService(
-                                         ApplicationContext context,
-                                         ObjectMapper objectMapper) {
+            ApplicationContext context,
+            ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
         this.beanFactory = context;
     }
 
-    public String handle(VkMessage message) throws JsonProcessingException, ErroneousResponseException {
+    public String handle(VkMessage message) {
         MessageSendQuery msgSendQuery = beanFactory.getBean(MessageSendQuery.class);
         String result = "";
         if (message.getPeerId() >= 2000000000)
             result = sendToGroup(message, msgSendQuery);
         else
             result = sendToPersonal(message, msgSendQuery);
-        JsonNode response = objectMapper.readTree(result);
-        if (response.has("error")) throw new ErroneousResponseException(response.get("Error").asText());
-            return result;
+        return result;
     }
 
     private String sendToGroup(VkMessage message, MessageSendQuery query) {
