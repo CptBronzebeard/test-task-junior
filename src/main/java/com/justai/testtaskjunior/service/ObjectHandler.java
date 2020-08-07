52fc02561ea3fdc0f619309e1c6a50c68d1c084c
cptbronzebeard <omg4itep@gmail.com>
2020-08-07 06:13:18 +0300

    Revert "Moved response error-checking logic out of BasicMessageNewHandlerService into CallbackController"
    This reverts commit edd396e8

--- src/main/java/com/justai/testtaskjunior/service/ObjectHandler.java
+++ src/main/java/com/justai/testtaskjunior/service/ObjectHandler.java
@@ -1,5 +1,8 @@
 package com.justai.testtaskjunior.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.justai.testtaskjunior.util.exceptions.ErroneousResponseException;
+
 public interface ObjectHandler<T> {
-    String handle(T object);
+    String handle(T object) throws JsonProcessingException, ErroneousResponseException;
 }
