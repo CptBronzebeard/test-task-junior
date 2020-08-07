d73e66404522986412f30a37e643eb907a5d90dd
cptbronzebeard <omg4itep@gmail.com>
2020-08-07 06:19:13 +0300

    Revert "Moved response error-checking logic into CallbackController"
    This partially reverts commit edd396e8

--- src/main/java/com/justai/testtaskjunior/controller/CallbackController.java
+++ src/main/java/com/justai/testtaskjunior/controller/CallbackController.java
@@ -1,7 +1,5 @@
 package com.justai.testtaskjunior.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.justai.testtaskjunior.service.CallbackHandlerService;
 import com.justai.testtaskjunior.util.exceptions.ErroneousResponseException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,21 +13,16 @@ import java.io.IOException;
 @Controller
 public class CallbackController {
     private final CallbackHandlerService callbackHandler;
-    private final ObjectMapper objectMapper;
 
     @Autowired
-    public CallbackController(CallbackHandlerService callbackHandler, ObjectMapper objectMapper) {
+    public CallbackController(CallbackHandlerService callbackHandler) {
         this.callbackHandler = callbackHandler;
-        this.objectMapper = objectMapper;
     }
 
     @RequestMapping(value = "${vk.callback}", method = RequestMethod.POST, consumes = {"application/json"})
     @ResponseStatus(HttpStatus.OK)
     public @ResponseBody
     String handleCallback(@RequestBody String incomingMessage) throws IOException, ErroneousResponseException {
-        String result = callbackHandler.handleCallback(incomingMessage);
-        JsonNode response = objectMapper.readTree(result);
-        if (response.has("error")) throw new ErroneousResponseException(response.get("Error").asText());
-        return result;
+        return callbackHandler.handleCallback(incomingMessage);
     }
 }
